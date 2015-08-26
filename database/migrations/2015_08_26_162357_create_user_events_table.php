727d1cc438091f653a2efbf08bbd0c466f69fe71
Alleo Indong <freedom_war550@live.com.ph>
2015-08-27 00:31:12 +0800

    Created Schema

---
+++ database/migrations/2015_08_26_162357_create_user_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('event_id')->unsigned();
+            $table->integer('bike_type_id')->unsigned();
+            $table->integer('event_category_id')->unsigned();
+            $table->string('tshirt_size', 10);
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_events');
+    }
+}
