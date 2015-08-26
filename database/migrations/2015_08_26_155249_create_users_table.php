6f28bcfb03dbc5845b7343ae1b6b47d31ec755d9
Alleo Indong <freedom_war550@live.com.ph>
2015-08-27 00:00:53 +0800

    Created schema

---
+++ database/migrations/2015_08_26_155249_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 150)->unique();
+            $table->string('password');
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('middlename', 50);
+            $table->timestamp('birthdate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
