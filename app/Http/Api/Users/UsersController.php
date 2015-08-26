7ebcb12e938805eb09a51c1c2bc7bd208efa139c
Alleo Indong <freedom_war550@live.com.ph>
2015-08-27 02:18:18 +0800

    Created userregistration service and events registration service

--- app/Http/Api/Users/UsersController.php
+++ app/Http/Api/Users/UsersController.php
@@ -1,15 +1,43 @@
 <?php
 namespace App\Http\Api\Users;
 
+use App\Http\Api\Users\Services\UserRegistration;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 
 class UsersController extends Controller
 {
+
+    protected $userRegistration;
+
+    public function __construct()
+    {
+        $this->userRegistration = new UserRegistration();
+    }
+
     public function index()
     {
         $user = User::all();
+        echo $this->userRegistration->register('hello');
+        return $user;
+    }
+
+    public function getUserById($id)
+    {
+        $user = User::find($id);
 
         return $user;
+    }
+
+    public function getUserByEmail($email)
+    {
+        $user = User::where('email', $email)->first();
+
+        return $user;
+    }
+
+    public function store()
+    {
+
     }
 }
