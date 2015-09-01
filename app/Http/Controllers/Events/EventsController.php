74311f4257f1810273bad185fcf4e40c1ebc073f
Alleo Indong <freedom_war550@live.com.ph>
2015-09-01 20:26:21 +0800

    Create user function

--- app/Http/Controllers/Events/EventsController.php
+++ app/Http/Controllers/Events/EventsController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\Events;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
+
+    protected $transactURL    = 'http://testpay.7-eleven.com.ph:8888/transact';
+    protected $inquireURL     = 'http:// testpay.7-eleven.com.ph:8888/inquire';
+    protected $transactionKey = 'fc9c9d077b2bb26d559dab5df8a457703ce14d894dc0ffb2dee7bf6dfaaff1f6';
+    protected $merchantID     = 'shimano_test';
+
     public function index()
     {
         return view('events.index');
@@ -24,35 +32,31 @@ class EventsController extends Controller
         return view('events.event')->with('event', $event);
     }
 
-    public function eventRegistration($slug)
+    public function eventRegistration(Request $request, $slug)
     {
+        $input = $request->all();
 
         $refNo = uniqid();
 
-        $merchantRef    = $refNo;
-        $amount         = '200';
-        $transactURL    = 'http://testpay.7-eleven.com.ph:8888/transact';
-        $inquireURL     = 'http:// testpay.7-eleven.com.ph:8888/inquire';
-        $transactionKey = 'fc9c9d077b2bb26d559dab5df8a457703ce14d894dc0ffb2dee7bf6dfaaff1f6';
-        $merchantID     = 'shimano_test';
-        $successURL     = url('/').'/events/'.$slug.'/success';
-        $failURL        = url('/').'/events/'.$slug.'/failed';
+        // REGISTER A USER
+        $user = $this->registerUser($input);
 
-        $token = sha1($merchantID . $merchantRef . '{' . $transactionKey . '}');
+        if (! $user) {
+            return redirect()
+                ->back()
+                ->withInput();
+        }
 
-        $fields = [
-            'merchantID'            => $merchantID,
-            'merchantRef'           => $merchantRef,
-            'amount'                => $amount,
-            'successURL'            => $successURL,
-            'failURL'               => $failURL,
-            'token'                 => $token,
-            'returnPaymentDetails'  => 'Y'
-        ];
+        // REGISTER A USER TO EVENT
+        $this->registerUserToEvent($user);
 
-        $params = http_build_query($fields);
+        // RECORD TRANSACTION
+        $this->recordTransaction($refNo, '7-Connect');
 
-        return redirect()->to($transactURL.'?'.$params);
+        // Get payment uri
+        $paymentUrl = $this->processPayment($refNo, $slug);
+
+        return redirect()->to($paymentUrl);
     }
 
     public function paymentSuccess($slug)
@@ -63,5 +67,57 @@ class EventsController extends Controller
     public function paymentFailed($slug)
     {
         echo 'Failed payment';
+    }
+
+    private function registerUser($input)
+    {
+        $user = null;
+
+        $user = new User;
+        $user->firstname    = $input['firstname'];
+        $user->lastname     = $input['lastname'];
+        $user->middlename   = $input['middlename'];
+        $user->birthdate    = strtotime($input['birthdate']);
+        $user->gender       = $input['gender'];
+        $user->mobilenumber = $input['mobilenumber'];
+        $user->nationality  = $input['nationality'];
+        $user->save();
+
+        return $user;
+    }
+
+    private function registerUserToEvent($user)
+    {
+
+    }
+
+    private function recordTransaction($refNo, $paymentMethod)
+    {
+
+    }
+
+    private function processPayment($refNo, $slug)
+    {
+        $merchantRef    = $refNo;
+        $amount         = '200';
+
+        $successURL     = url('/').'/events/'.$slug.'/success';
+        $failURL        = url('/').'/events/'.$slug.'/failed';
+
+        $token = sha1($this->merchantID . $merchantRef . '{' . $this->transactionKey . '}');
+
+        $fields = [
+            'merchantID'            => $this->merchantID,
+            'merchantRef'           => $merchantRef,
+            'amount'                => $amount,
+            'successURL'            => $successURL,
+            'failURL'               => $failURL,
+            'token'                 => $token,
+            'returnPaymentDetails'  => 'Y'
+        ];
+
+        $params = http_build_query($fields);
+
+        return $this->transactURL.'?'.$params;
     }
 }
