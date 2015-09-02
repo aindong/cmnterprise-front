d1a1d06178496363c995f7e3af6eeca1c4e7682c
Alleo Indong <freedom_war550@live.com.ph>
2015-09-02 17:56:30 +0800

    updated menu

--- resources/views/layouts/front2.blade.php
+++ resources/views/layouts/front2.blade.php
@@ -18,12 +18,12 @@
     <nav>
         <ul class="left" style="margin-left: 20px"><img src="/img/logo1.png" alt="" style="width: 300px;height: auto;vertical-align: middle;"></ul>
         <ul class="right hide-on-med-and-down">
-            <li><a href="#!">First Sidebar Link</a></li>
-            <li><a href="#!">Second Sidebar Link</a></li>
+            <li><a href="#!">Home</a></li>
+            <li><a href="#!">Events</a></li>
         </ul>
         <ul id="slide-out" class="side-nav">
-            <li><a href="#!">First Sidebar Link</a></li>
-            <li><a href="#!">Second Sidebar Link</a></li>
+            <li><a href="#!">Home</a></li>
+            <li><a href="#!">Events</a></li>
         </ul>
         <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
     </nav>
