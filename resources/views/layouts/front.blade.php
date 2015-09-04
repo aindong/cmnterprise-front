30402a2bead7f897bf734822d889a76e810db130
Alleo Indong <freedom_war550@live.com.ph>
2015-09-04 20:40:55 +0800

    updated front

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -64,6 +64,26 @@
     </div>
 </footer>
 
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script type="text/javascript" src="/js/jquery-1.10.2.js"></script>
 <script type="text/javascript" src="/js/bootstrap.js"></script>
 <script type="text/javascript" src="/js/jquery.flexslider.js"></script>
@@ -74,6 +94,12 @@
 <script type="text/javascript" src="/js/responsiveCarousel.js"></script>
 <script type="text/javascript" src="/js/settings.js"></script>
 <script type="text/javascript" src="/js/forms.js"></script>
+
+<script>
+    $(function() {
+        $('#myModal').modal('show');
+    });
+</script>
 
 </body>
 </html>
