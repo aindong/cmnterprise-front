e0bfd79c722f47c5aa76cce9b369a6bef583f4c6
Alleo Indong <freedom_war550@live.com.ph>
2015-09-04 20:45:05 +0800

    added modal

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -71,14 +71,15 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <h4 class="modal-title" id="myModalLabel">Current Event</h4>
             </div>
             <div class="modal-body">
-                ...
+                <img src="/img/events_banner_1.jpg" alt=""/>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+
+                <a href="/events/shimano-gran-fondo-2015" role="button" class="btn btn-primary">Register for this event</a>
             </div>
         </div>
     </div>
